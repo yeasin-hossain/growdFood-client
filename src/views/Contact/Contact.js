e8c03f8abbd4dfc29ad4070f92f909208ee550bd
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-04-17 20:39:46 +0600

    add contact form work

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-return-assign */
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
+import { toast } from 'react-toastify';
 import contactIcon from '../../Images/contact.svg';
 
 const customStyles = {
@@ -28,12 +30,31 @@ function Contact() {
         right: '3%',
         zIndex: '999999999',
     };
+
+    const [contactData, setContactData] = useState({});
+    const submitContactData = async () => {
+        try {
+            const saveContactData = await axios.post(
+                `${process.env.REACT_APP_API_BASE_URL}api/public/contact`,
+                contactData
+            );
+            if (saveContactData.data.message) {
+                toast.error(saveContactData.data.message);
+                return;
+            }
+            toast.success('We Will ASAP Contact With You, Thanks!');
+            closeModal();
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <div style={contactStyles}>
             <button className="btn rounded-circle" onClick={openModal} type="button">
                 <img src={contactIcon} alt="" style={{ height: '45px' }} />
             </button>
             <Modal
+                ariaHideApp={false}
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
                 style={customStyles}
@@ -42,17 +63,27 @@ function Contact() {
                 <div>Email</div>
                 <div className="mb-3">
                     <input
+                        required
                         type="email"
                         className="form-control"
                         id="exampleFormControlInput1"
                         placeholder="name@example.com"
+                        onChange={(e) =>
+                            setContactData({ ...contactData, userEmail: e.target.value })
+                        }
                     />
                 </div>
                 <div>Write Your Query</div>
                 <div className="mb-3">
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" />
+                    <textarea
+                        className="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="3"
+                        required
+                        onChange={(e) => setContactData({ ...contactData, query: e.target.value })}
+                    />
                 </div>
-                <button className="btn btn-warning" type="button">
+                <button className="btn btn-warning" type="button" onClick={submitContactData}>
                     Submit
                 </button>
                 <button onClick={closeModal} type="button" className="btn btn-danger ms-5">
