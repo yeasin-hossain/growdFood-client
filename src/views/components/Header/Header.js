3445bd00d6041f71aae327a918c2224d5745c886
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-04-18 03:54:54 +0600

    change nav

--- src/views/components/Header/Header.js
+++ src/views/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
 import { GrowContext } from '../../../context/GrowContext';
 import dashboardIcon from '../../../Images/dashboard.svg';
@@ -17,10 +18,10 @@ function Header() {
         setLoggedIn(false);
         history.push('/');
     };
-    const { role } = currentUser;
+    const { role, name } = currentUser;
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top px-5">
-            <div className="container-fluid">
+        <Navbar bg="light" expand="lg" className="sticky-top">
+            <Navbar.Brand href="#home">
                 <Link
                     className="nav-link "
                     data-bs-toggle="tooltip"
@@ -30,111 +31,97 @@ function Header() {
                 >
                     <img src={Logo} alt="" className="img-fluid" style={{ width: '50px' }} />
                 </Link>
-
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div
-                    className="collapse navbar-collapse justify-content-center"
-                    id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link>
+                        <Link
+                            className="nav-link mx-3"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="bottom"
+                            title="Home"
+                            to="/"
+                        >
+                            <img
+                                src={HomeIcon}
+                                alt=""
+                                className="img-fluid"
+                                style={{ width: '30px' }}
+                            />
+                        </Link>
+                    </Nav.Link>
+                    <Nav.Link href="#link">
+                        {role === 'admin' ? (
                             <Link
                                 className="nav-link mx-3"
                                 data-bs-toggle="tooltip"
                                 data-bs-placement="bottom"
-                                title="Home"
-                                to="/"
+                                title="Profile"
+                                to="/admin/orders"
                             >
                                 <img
-                                    src={HomeIcon}
+                                    src={dashboardIcon}
                                     alt=""
                                     className="img-fluid"
                                     style={{ width: '30px' }}
                                 />
                             </Link>
-                        </li>
-                        <li className="nav-item">
-                            {role === 'admin' ? (
-                                <Link
-                                    className="nav-link mx-3"
-                                    data-bs-toggle="tooltip"
-                                    data-bs-placement="bottom"
-                                    title="Profile"
-                                    to="/admin/orders"
-                                >
-                                    <img
-                                        src={dashboardIcon}
-                                        alt=""
-                                        className="img-fluid"
-                                        style={{ width: '30px' }}
-                                    />
-                                </Link>
-                            ) : (
-                                <Link
-                                    className="nav-link mx-3"
-                                    data-bs-toggle="tooltip"
-                                    data-bs-placement="bottom"
-                                    title="DashBoard"
-                                    to="/user/order"
-                                >
-                                    <img
-                                        src={ProfileIcon}
-                                        alt=""
-                                        className="img-fluid"
-                                        style={{ width: '30px' }}
-                                    />
-                                </Link>
-                            )}
-                        </li>
-
-                        <li className="nav-item">
-                            {isLoggedIn ? (
-                                <button
-                                    className="btn"
-                                    type="button"
-                                    data-bs-toggle="tooltip"
-                                    data-bs-placement="bottom"
-                                    title="Logout"
-                                    onClick={logoutBtn}
-                                >
-                                    <img
-                                        src={logoutIcon}
-                                        alt=""
-                                        className="img-fluid"
-                                        style={{ width: '40px' }}
-                                    />
-                                </button>
-                            ) : (
-                                <Link
-                                    className="nav-link "
-                                    data-bs-toggle="tooltip"
-                                    data-bs-placement="bottom"
-                                    title="Login"
-                                    to="/auth"
-                                >
-                                    <img
-                                        src={loginIcon}
-                                        alt=""
-                                        className="img-fluid"
-                                        style={{ width: '40px' }}
-                                    />
-                                </Link>
-                            )}
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                        ) : (
+                            <Link
+                                className="nav-link mx-3"
+                                data-bs-toggle="tooltip"
+                                data-bs-placement="bottom"
+                                title="DashBoard"
+                                to="/user/order"
+                            >
+                                <img
+                                    src={ProfileIcon}
+                                    alt=""
+                                    className="img-fluid"
+                                    style={{ width: '30px' }}
+                                />
+                            </Link>
+                        )}
+                    </Nav.Link>
+                    <Nav.Link href="#link">
+                        {isLoggedIn ? (
+                            <button
+                                className="btn"
+                                type="button"
+                                data-bs-toggle="tooltip"
+                                data-bs-placement="bottom"
+                                title="Logout"
+                                onClick={logoutBtn}
+                            >
+                                <img
+                                    src={logoutIcon}
+                                    alt=""
+                                    className="img-fluid"
+                                    style={{ width: '40px' }}
+                                />
+                            </button>
+                        ) : (
+                            <Link
+                                className="nav-link "
+                                data-bs-toggle="tooltip"
+                                data-bs-placement="bottom"
+                                title="Login"
+                                to="/auth"
+                            >
+                                <img
+                                    src={loginIcon}
+                                    alt=""
+                                    className="img-fluid"
+                                    style={{ width: '40px' }}
+                                />
+                            </Link>
+                        )}
+                    </Nav.Link>
+                    {name && <p className="text-danger">{name}❤</p>}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
